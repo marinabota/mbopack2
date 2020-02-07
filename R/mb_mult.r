f3575831d8b2d5c606e9fb600656b621608076c0
marinabota <maria.bota@gmail.com>
2020-02-07 16:13:30 +0100

    Added scripts

---
+++ R/mb_mult.r
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param x 
+#' @param y 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+mb_mult=function(x,y){
+  return(x*y)
+}
---
+++ R/mb_sum.r
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param x 
+#' @param y 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+mb_sum=function(x,y){
+  return (x+y)
+}
